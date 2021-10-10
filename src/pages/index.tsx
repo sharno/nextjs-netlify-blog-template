d96a1a2faf2556890d5c82f891eb0f867571db85
Mohamed Elsharnouby <sharnoby3@gmail.com>
2021-10-10 02:35:37 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,11 @@ export default function Index() {
             Hi, I'm Mohamed Elsharnouby<span className="fancy">.</span>
           </h1>
           <span className="handle">sharno.netlify.app</span>
-          <h2>A blog about things I'm interested in. I'm a muslim 🕌 software engineer 💻. A love for writing good and bug free software using type systems that watch my back and immutability that stops me from doing stupid things. I love outdoors 🌄 and hiking 🚶‍♂️.</h2>
+          <h2>A blog about things I'm interested in.
+            I'm a muslim 🕌 software engineer 💻.
+            A love for writing good and bug free software using type systems that watch my back
+              and immutability that stops me from doing stupid things.
+            I love outdoors 🌄 and hiking 🚶‍♂️.</h2>
           <SocialList />
         </div>
       </div>
